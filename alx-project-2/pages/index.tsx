0cfdc369b690a5ae62e3821fe26f9ec496e567f2
hashim-baya <bayahashim40@gmail.com>
2025-07-22 22:59:09 +0300

    Deleted previous directory that changed

--- alx-project-2/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-const HomePage: React.FC = () => {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main entry point of our application.</p>
-    </div>
-  );
-}
-export default HomePage;
